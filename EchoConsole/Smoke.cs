b8a64da8676caaefb03f21939735859f2be4d216
Erick Segaar <zwarebats@gmail.com>
2019-01-25 10:15:28 +0100

    Added selfdiagnostics

---
+++ EchoConsole/Smoke.cs
@@ -0,0 +1,23 @@
+﻿using SelfDiagnosis;
+using System;
+
+namespace EchoConsole
+{
+    class Smoke : SmokeBase
+    {
+        public override bool ValidateAppSetting(object item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool ValidateConfigSetting(object item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void ValidateOther()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
